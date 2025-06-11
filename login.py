3092bbe3177d56562ec7d2b860870c8b2877492b
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-06-11 07:12:00 -0500

    first commit

---
+++ login.py
@@ -0,0 +1,10 @@
+# ------------- librerias -------------- #
+
+from tkinter import *                           # para crear la interfaz grafica
+import os                                       # para interactuar con el sistema operativo
+import cv2                                      # para leer y mostrar imagenes
+from matplotlib import pyplot                   # para mostrar imagenes
+from mtcnn.mtcsnn import MTCNN                  # para detectar rostros en imagenes
+import numpy as np                              # para realizar operaciones matematicas
+
+# ------------- Funciones -------------- #
